a70f401f2a4f293ef5fd8600ceaaa0294c3c48a5
Random Code Guy <129107933+Random-Code-Guy@users.noreply.github.com>
2024-07-19 18:37:02 +0100

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ from utils import start_update_thread
 if __name__ == "__main__":
     if not os.path.exists('maps'):
         os.makedirs('maps')
+    if not os.path.exists('assets'):
+        os.makedirs('assets')
         
     root = ttk.Window(themename="superhero")
     app = DepthMapApp(root)
